79fe707b10474ffba52932068b643d3fe1420fa9
brangarten <brandonjgarcia1024@gmail.com>
2025-06-25 00:49:40 -0400

    11 :P

---
+++ assignments/Assignment 5/11.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+bool isPrime(int x);
+
+int main() {
+
+    int x = 5;
+
+    cout << isPrime(25);
+    cout << isPrime(5);
+    cout << isPrime(1);
+
+
+    return 0;
+}
+
+bool isPrime(int x) {
+    
+    if (SMD) {
+        return false;
+    } else if (x % x == 0) {
+        return true;
+    }
+}
